ca74422dc868b850ea7eb15d5e83d6ccde472d8a
Cyril Suzat <cyril.suzat@me.com>
2023-03-15 16:36:27 +0100

    Mise en place du Header

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,10 +1,13 @@
 <script>
+  import Header from "./Header.svelte";
   import Hero from "./Hero.svelte";
 </script>
 
 <svelte:head>
   <title>OUT'CHA - Sport for All</title>
-  <meta name="description" content="Association qui a pour but de favoriser l'inclusion sociale et la réalisation de rêves par le sport." />
+  <meta name="description"
+        content="Association qui a pour but de favoriser l'inclusion sociale et la réalisation de rêves par le sport." />
 </svelte:head>
 
+<Header />
 <Hero />
---
+++ src/routes/home/Header.svelte
@@ -0,0 +1,79 @@
+<script>
+  import logo from '$lib/images/logo.jpg';
+</script>
+
+<div class="header">
+  <h1 class="logo">
+    <a href="/"><img src={logo} alt="Logo" /></a>
+  </h1>
+
+  <ul class="nav">
+    <li><a href="/">Accueil</a></li>
+    <li><a href="/about">L'association</a></li>
+    <li><a href="/projects">Les projets</a></li>
+    <li><a href="/team">La Team</a></li>
+    <li><a href="/contact">Contact</a></li>
+  </ul>
+</div>
+
+<style lang="sass">
+  @use '../mixins' as *
+
+  .header
+    background-color: #fff
+    display: flex
+    align-items: center
+    padding: 15px 0 5px 0
+    justify-content: center
+
+    +tablet
+      padding: 25px 4% 10px 4%
+      justify-content: space-between
+
+    +desktop
+      padding: 25px 45px 10px 45px
+
+  .logo
+    margin: 0
+
+    img
+      height: 55px
+
+      +tablet
+        height: 55px
+
+      +desktop
+        height: 70px
+
+  .nav
+    list-style: none
+    margin: 0
+    padding: 0
+    display: none
+
+    +tablet
+      display: flex
+
+    li
+      padding: 0 10px 10px 10px
+      font-size: 16px
+      letter-spacing: -1px
+
+      +desktop
+        font-size: 18px
+        padding: 0 12px 12px 12px
+
+    a
+      text-decoration: none
+      color: #333
+      text-transform: uppercase
+      line-height: 40px
+      padding: 10px 0
+
+      &:hover
+        border-bottom: 2px solid #ff1130
+
+      &.router-link-active
+        border-bottom: 2px solid #ff1130
+        color: #ff1130
+</style>
