1b29152c5f6eaf424cea70e14af72b70cd01104d
Cyril Suzat <cyril.suzat@me.com>
2023-04-17 11:15:37 +0200

    Ajout d'informations sur la page About

--- src/routes/association/+page.svelte
+++ src/routes/association/+page.svelte
@@ -4,6 +4,7 @@
   import Footer from "../Footer.svelte";
 
   import banner from '$lib/images/association/banner.webp';
+  import Icon from '@iconify/svelte';
 </script>
 
 <svelte:head>
@@ -16,26 +17,107 @@
 <Banner title="L'association" image={banner} />
 
 <div class="wrapper">
-  <div class="container">
-    <h2>Qui sommes-nous ?</h2>
-    <p>
-      L’association a pour but de favoriser l’inclusion sociale,
-      l’épanouissement, la réalisation de rêves et le partage par le sport,
-      d’encourager la pratique du sport dans le respect de la nature,
-      de promouvoir les échanges socio-culturels et l’intégration
-      des valeurs sportives, chez les jeunes à travers le sport et les défis.
-    </p>
-  </div>
+  <section class="about">
+    <div class="container">
+      <h1><em>Outdoor</em> Challenges</h1>
+      <p>
+        L’association a pour but de favoriser l’inclusion sociale,
+        l’épanouissement, la réalisation de rêves et le partage par le sport,
+        d’encourager la pratique du sport dans le respect de la nature,
+        de promouvoir les échanges socio-culturels et l’intégration
+        des valeurs sportives, chez les jeunes à travers le sport et les défis.
+      </p>
+
+      <p>
+        <a href="/projets" class="button">
+          Voir tous les projets
+          <span class="icon"><Icon icon="material-symbols:arrow-forward-ios" /></span>
+        </a>
+      </p>
+
+      <h2>Aidez nous</h2>
+      <div class="card">
+        <h3>Pour soutenir le projet:</h3>
+        IBAN: CH53 0076 7000 S546 2495 4<br>
+        Numéro TWINT : 078 958 06 26
+      </div>
+    </div>
+  </section>
 </div>
 
 <Footer />
 
 <style lang="sass">
+  @use '../../lib/sass/variables' as *
+
   .wrapper
     background-color: #fff
+    background-image: url("../../lib/images/bg-green-dots.jpg")
+    background-size: cover
     padding: 30px 0
     min-height: 350px
 
-  p
-    margin: 0 0 15px 0
+  h1
+    font-family: 'Anton', sans-serif
+    font-weight: 400
+    font-size: 55px
+    text-transform: uppercase
+    line-height: 1
+    margin: 24px 0
+    text-align: center
+
+    em
+      font-family: 'Great Sejagad', sans-serif
+      color: $primary
+      display: block
+      font-size: 45px
+      font-weight: 400
+      line-height: .9
+      position: relative
+
+  h2
+    font-family: 'Anton', sans-serif
+    font-weight: 400
+    font-size: 24px
+    text-transform: uppercase
+    line-height: 1
+    margin: 0
+    text-align: center
+
+  h3
+    margin: 12px 0
+
+  .button
+    font-size: 16px
+    font-weight: bold
+    padding: 15px 20px
+    background-color: $secondary
+    color: #fff
+    text-decoration: none
+    text-transform: uppercase
+    text-shadow: 0 0 3px #666
+    display: inline-block
+    line-height: normal
+
+    .icon
+      position: relative
+      bottom: -2px
+      right: -2px
+
+    &:hover
+      background-color: #d9441c
+
+  .about
+    padding-bottom: 30px
+
+    p
+      margin: 40px 0
+      font-size: 18px
+      font-weight: 500
+      line-height: 30px
+      text-align: center
+
+    .card
+      text-align: center
+      line-height: 24px
 </style>
