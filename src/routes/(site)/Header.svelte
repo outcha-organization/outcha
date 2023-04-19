8f68d258f6f89e44528741d2b1018764b1fa9299
Cyril Suzat <cyril.suzat@me.com>
2023-04-19 11:49:06 +0200

    Corrige la fermeture de la nav mobile

--- src/routes/(site)/Header.svelte
+++ src/routes/(site)/Header.svelte
@@ -10,19 +10,23 @@
   function toggleNav() {
     showNav = !showNav;
   }
+
+  function closeNav() {
+    showNav = false;
+  }
 </script>
 
 <div id="header">
   <h1 id="logo">
-    <a href="/home"><img src={logo} alt="Logo" /></a>
+    <a href="/home" on:click={closeNav}><img src={logo} alt="Logo" /></a>
   </h1>
 
   <ul id="nav" class:visible={showNav}>
-    <li><a href="/home" on:click={toggleNav} class:active={$page.url.pathname.includes('/home')}>Accueil</a></li>
-    <li><a href="/association" on:click={toggleNav} class:active={$page.url.pathname.includes('/association')}>L'association</a></li>
-    <li><a href="/projets" on:click={toggleNav} class:active={$page.url.pathname.includes('/projets')}>Les projets</a></li>
-    <li><a href="/team" on:click={toggleNav} class:active={$page.url.pathname.includes('/team')}>La Team</a></li>
-    <li><a href="/contact" on:click={toggleNav} class:active={$page.url.pathname.includes('/contact')}>Contact</a></li>
+    <li><a href="/home" on:click={closeNav} class:active={$page.url.pathname.includes('/home')}>Accueil</a></li>
+    <li><a href="/association" on:click={closeNav} class:active={$page.url.pathname.includes('/association')}>L'association</a></li>
+    <li><a href="/projets" on:click={closeNav} class:active={$page.url.pathname.includes('/projets')}>Les projets</a></li>
+    <li><a href="/team" on:click={closeNav} class:active={$page.url.pathname.includes('/team')}>La Team</a></li>
+    <li><a href="/contact" on:click={closeNav} class:active={$page.url.pathname.includes('/contact')}>Contact</a></li>
   </ul>
 
   <div id="nav-toggler">
