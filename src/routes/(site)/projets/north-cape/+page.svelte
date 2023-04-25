abc348821408c609f821fa4df7fb5308aea9179c
Cyril Suzat <cyril.suzat@me.com>
2023-04-25 10:14:38 +0200

    MAJ page North Cape

--- src/routes/(site)/projets/north-cape/+page.svelte
+++ src/routes/(site)/projets/north-cape/+page.svelte
@@ -5,16 +5,23 @@
   import banner from '$lib/images/projets/bg-dark.webp';
   import Icon from '@iconify/svelte';
 
+  import cover from '$lib/images/projets/north-cape/cover.webp';
   import photo1 from '$lib/images/projets/north-cape/photo1.webp';
   import photo2 from '$lib/images/projets/north-cape/photo2.webp';
   import photo3 from '$lib/images/projets/north-cape/photo3.webp';
   import photo4 from '$lib/images/projets/north-cape/photo4.webp';
+  import photo5 from '$lib/images/projets/north-cape/photo5.webp';
+  import photo6 from '$lib/images/projets/north-cape/photo6.webp';
+  import photo7 from '$lib/images/projets/north-cape/photo7.webp';
 
   const images = [
-    { image: photo1, width: 960, height: 720 },
+    { image: photo1, width: 1200, height: 1600 },
     { image: photo2, width: 1600, height: 1200 },
-    { image: photo3, width: 1230, height: 2048 },
-    { image: photo4, width: 1600, height: 1200 }
+    { image: photo3, width: 1200, height: 1600 },
+    { image: photo4, width: 1600, height: 1200 },
+    { image: photo5, width: 1200, height: 1600 },
+    { image: photo6, width: 1600, height: 1200 },
+    { image: photo7, width: 1600, height: 1200 },
   ]
 </script>
 
@@ -36,25 +43,42 @@
 
       <div class="location">Norvège</div>
       <h1>North Cape</h1>
-      <h3>5500 kilomètre à vélo jusqu'en Norvège</h3>
+      <h2>5500 kilomètre à vélo jusqu'en Norvège</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis deleniti dolorum exercitationem molestiae
         odio quisquam reprehenderit! A ad debitis eaque fugit harum illo laborum modi molestias officia praesentium,
         repudiandae suscipit?</p>
       <div class="cover">
-        <img src={photo1} alt="North Cape" />
+        <img src={cover} alt="North Cape" />
       </div>
-      <article>
+
+      <section>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis deleniti dolorum exercitationem molestiae
           odio quisquam reprehenderit! A ad debitis eaque fugit harum illo laborum modi molestias officia praesentium,
           repudiandae suscipit?</p>
-        <img src={photo3} alt="North Cape" width="240" class="is-pulled-left" />
+        <h3>Lorem Ipsum</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis deleniti dolorum exercitationem molestiae
           odio quisquam reprehenderit! A ad debitis eaque fugit harum illo laborum modi molestias officia praesentium,
           repudiandae suscipit?</p>
+        <img src={photo3} alt="North Cape" class="is-pulled-left" />
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias architecto at corporis, cum delectus deleniti
           dolor dolores ea eos expedita, explicabo facere magnam magni provident quia ratione rerum saepe
           voluptatibus?</p>
-      </article>
+        <h4>Dolor sit amet</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias architecto at corporis, cum delectus deleniti
+          dolor dolores ea eos expedita, explicabo facere magnam magni provident quia ratione rerum saepe
+          voluptatibus?</p>
+      </section>
+
+      <section>
+        <h3>Alias architecto at corporis</h3>
+        <img src={photo6} alt="North Cape" class="is-pulled-right" />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias architecto at corporis, cum delectus deleniti
+          dolor dolores ea eos expedita, explicabo facere magnam magni provident quia ratione rerum saepe
+          voluptatibus?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias architecto at corporis, cum delectus deleniti
+          dolor dolores ea eos expedita, explicabo facere magnam magni provident quia ratione rerum saepe
+          voluptatibus?</p>
+      </section>
 
       <p class="more">
         <a href="/projets">
@@ -117,16 +141,16 @@
         color: $secondary
 
   h2
-    font-family: 'Anton', sans-serif
-    text-transform: uppercase
     font-size: 24px
-    letter-spacing: 1px
-    line-height: 70px
-    margin: 0
+    margin: 0 0 15px 0
 
   h3
-    font-size: 24px
-    margin: 0
+    font-size: 18px
+    margin: 0 0 15px 0
+
+  h4
+    font-size: 16px
+    margin: 0 0 15px 0
 
   .cover
     margin: 20px 0
@@ -135,21 +159,39 @@
       max-width: 100%
 
   p
-    line-height: 24px
+    margin: 15px 0
+    line-height: 28px
+    color: #333
 
-  article
+  section
+    margin-bottom: 15px
+
     &::after
       clear: both
       content: " "
       display: table
 
-    .is-pulled-left
-      float: left
-      margin-right: 20px
+  .is-pulled-left
+    width: 100%
 
-    .is-pulled-right
+    +tablet
+      width: 230px
+      float: left
+      margin: 5px 20px 5px 0
+
+    +desktop
+      width: 250px
+
+  .is-pulled-right
+    width: 100%
+
+    +tablet
+      width: 230px
       float: right
-      margin-left: 20px
+      margin: 5px 0 5px 20px
+
+    +desktop
+      width: 250px
 
   .more
     border-top: solid 1px $grey
