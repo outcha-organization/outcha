1e899341d0c6d5ee464314ba76d7be6b7030441d
Cyril Suzat <cyril.suzat@me.com>
2023-05-01 16:52:38 +0200

    Page "contact"

--- src/routes/(site)/contact/+page.svelte
+++ src/routes/(site)/contact/+page.svelte
@@ -13,22 +13,62 @@
 
 <div class="wrapper">
   <div class="container">
-    <h2>Contact</h2>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque doloremque exercitationem nesciunt omnis
-      repellendus
-      repudiandae rerum velit voluptate! Excepturi fuga libero magni obcaecati officia? Cumque dolorem iusto maxime quas
-      quo.
-    </p>
+    <div class="columns gap-6">
+      <div class="column">
+        <h1>Nous contacter</h1>
+        <p><strong>L'association OUT'CHA est basée à Lausanne</strong></p>
+        <p>
+          C/o Oral design int<br>
+          Rue Centrale 6<br>
+          1003 Lausanne, Suisse
+        </p>
+        <p><a href="mailto:info@outcha.org" class="button is-small">info@outcha.org</a></p>
+      </div>
+      <div class="column">
+        <h2>Nous soutenir</h2>
+        <p>
+          IBAN: CH53 0076 7000 S546 2495 4<br>
+          Numéro TWINT : 078 958 06 26
+        </p>
+        <p class="mb-5"><small>Attestation de don sur demande</small></p>
+
+        <h3>Par téléphone:</h3>
+        Hervé: 078 858 58 69 / Victor: 079 212 59 66
+      </div>
+    </div>
   </div>
 </div>
 
 <style lang="sass">
+  @use '$lib/sass/utilities/variables' as *
+
   .wrapper
     background-color: #fff
     padding: 30px 0
     min-height: 350px
+    text-align: center
+
+  h1
+    font-family: 'Anton', sans-serif
+    font-weight: 400
+    font-size: 32px
+    text-transform: uppercase
+    margin-bottom: 24px
+
+  h2
+    font-family: 'Anton', sans-serif
+    font-weight: 400
+    font-size: 24px
+    text-transform: uppercase
+    margin-bottom: 24px
+
+  h3
+    font-weight: 600
+    font-size: 16px
+    margin-bottom: 10px
 
   p
-    margin: 0 0 15px 0
+    font-weight: 400
+    line-height: 24px
+    margin-bottom: 16px
 </style>
