18199568acaca0aa5f7c3085e9435f10f34d9103
Cyril Suzat <cyril.suzat@me.com>
2023-05-02 12:14:21 +0200

    Ajout de photos sur la page association

--- src/routes/(site)/association/+page.svelte
+++ src/routes/(site)/association/+page.svelte
@@ -1,7 +1,49 @@
 <script>
+  import { onMount } from 'svelte';
+  import Swiper from 'swiper';
+  import 'swiper/css';
   import Banner from "../Banner.svelte";
   import banner from '$lib/images/association/banner.webp';
   import Icon from '@iconify/svelte';
+
+  import grandToGrand1 from '$lib/images/association/projects/grand-to-grand-1.webp';
+  import grandToGrand2 from '$lib/images/association/projects/grand-to-grand-2.webp';
+  import grandToGrand3 from '$lib/images/association/projects/grand-to-grand-3.webp';
+  import lapland1 from '$lib/images/association/projects/lapland-1.webp';
+  import lapland2 from '$lib/images/association/projects/lapland-2.webp';
+  import lapland3 from '$lib/images/association/projects/lapland-3.webp';
+  import mds1 from '$lib/images/association/projects/mds-1.webp';
+  import mds2 from '$lib/images/association/projects/mds-2.webp';
+  import mds3 from '$lib/images/association/projects/mds-3.webp';
+  import yukon1 from '$lib/images/association/projects/yukon-1.webp';
+  import yukon2 from '$lib/images/association/projects/yukon-2.webp';
+  import yukon3 from '$lib/images/association/projects/yukon-3.webp';
+
+  onMount(() => {
+    new Swiper('.swiper', {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next'
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 5,
+          spaceBetween: 35,
+        },
+        1024: {
+          slidesPerView: 6,
+          spaceBetween: 20,
+        }
+      }
+    });
+  })
 </script>
 
 <svelte:head>
@@ -23,6 +65,24 @@
         de promouvoir les échanges socio-culturels et l’intégration
         des valeurs sportives, chez les jeunes à travers le sport et les défis.
       </p>
+
+      <div class="swiper mySwiper">
+        <div class="swiper-wrapper">
+          <a href="/projets/grand-to-grand" class="swiper-slide"><img src={grandToGrand2} /></a>
+          <a href="/projets/mds" class="swiper-slide"><img src={mds3} /></a>
+          <a href="/projets/laponie" class="swiper-slide"><img src={lapland2} /></a>
+          <a href="/projets/grand-to-grand" class="swiper-slide"><img src={grandToGrand1} /></a>
+          <a href="/projets/laponie" class="swiper-slide"><img src={lapland3} /></a>
+          <a href="/projets/grand-to-grand" class="swiper-slide"><img src={grandToGrand3} /></a>
+          <a href="/projets/mds" class="swiper-slide"><img src={mds2} /></a>
+          <a href="/projets/laponie" class="swiper-slide"><img src={lapland1} /></a>
+          <a href="/projets/mds" class="swiper-slide"><img src={mds1} /></a>
+          <a href="/projets/yukon" class="swiper-slide"><img src={yukon3} /></a>
+          <a href="/projets/yukon" class="swiper-slide"><img src={yukon2} /></a>
+          <a href="/projets/yukon" class="swiper-slide"><img src={yukon1} /></a>
+        </div>
+        <div class="swiper-pagination"></div>
+      </div>
 
       <p>
         <a href="/projets" class="button">
@@ -57,7 +117,7 @@
     font-size: 55px
     text-transform: uppercase
     line-height: 1
-    margin: 24px 0
+    margin: 0 0 24px 0
     text-align: center
 
     em
